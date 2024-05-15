69175fb8fb91bd1ec0467ea3d4ab19557e356500
Dipongkar Roy <dipongkarroy233@gmail.com>
2024-05-15 11:06:52 +0600

    express mastering

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -41,5 +50,28 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     console.log(req.body);
     res.send('amr sonar bangla');
+});
+app.get("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send(someOne);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.all("*", (req, res) => {
+    res.status(400).json({
+        success: false,
+        massage: 'Router not found data'
+    });
+});
+//global error handler :
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            massage: 'error handle data'
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 
-import express, { Request, Response } from "express"
+import { error } from "console";
+import express, { NextFunction, Request, Response } from "express"
 
 const app = express()
 const port = 3000
@@ -43,6 +44,33 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.send('amr sonar bangla')
+})
+
+
+app.get("/", async(req:Request,res:Response ,next:NextFunction)=>{
+ try{
+  res.send(someOne)
+ }catch(error){
+  next(error)
+ }
+ 
+ })
+
+app.all("*",(req:Request ,res:Response)=>{
+  res.status(400).json({
+    success:false ,
+    massage:'Router not found data'
+  })
+})
+
+//global error handler :
+app.use((error:any ,req:Request,res:Response,next:NextFunction)=>{
+  if(error){
+    res.status(400).json({
+      success:false ,
+      massage:'error handle data'
+    })
+  }
 })
 
 
